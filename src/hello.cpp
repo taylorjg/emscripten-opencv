da924723d02c751318e293b6e26a0a861adb13c6
Administrator <jonathan.taylor.programmer@gmail.com>
2019-12-10 13:31:11 +0000

    Return image data from C++/WASM to JavaScript

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
@@ -8,11 +9,15 @@ extern "C" {
 #endif
 
 EMSCRIPTEN_KEEPALIVE
-void processImage(uchar *array, int width, int height) {
+uchar *processImage(uchar *array, int width, int height) {
   cv::Mat mat(height, width, CV_8UC4, array);
-  EM_ASM_({
-    console.log(`[processImage] width: ${$0}; height: ${$1}; array[0]: ${$2}; array[1]: ${$3}; array[2]: ${$4}`)
-  }, width, height, array[0], array[1], array[2]);
+  // EM_ASM_({
+  //   console.log(`[processImage] width: ${$0}; height: ${$1}; array[0]: ${$2}; array[1]: ${$3}; array[2]: ${$4}`)
+  // }, width, height, array[0], array[1], array[2]);
+  const int cb = width * height * 4;
+  uchar *array_copy = reinterpret_cast<uchar*>(malloc(cb));
+  std::memcpy(array_copy, array, cb);
+  return array_copy;
 }
 
 #ifdef __cplusplus
--- src/index.js
+++ src/index.js
@@ -6,27 +6,44 @@ window.Module = {
 }
 
 const processImageBtn = document.getElementById('process-image-btn')
-const img = document.getElementById('input-image')
+const inputImage = document.getElementById('input-image')
+const outputImage = document.getElementById('output-image')
 
 const getImageData = () => {
+  console.log('[getImageData]')
   const canvas = document.createElement('canvas')
-  canvas.width = img.width
-  canvas.height = img.height
+  canvas.width = inputImage.width
+  canvas.height = inputImage.height
   ctx = canvas.getContext('2d')
-  ctx.drawImage(img, 0, 0, img.width, img.height)
+  ctx.drawImage(inputImage, 0, 0, inputImage.width, inputImage.height)
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
   return imageData
 }
 
-const onProcessImage = processImage => () => {
+const drawOutputImage = (dataOut, width, height) => {
+  console.log('[drawOutputImage]')
+  const array = new Uint8ClampedArray(dataOut)
+  const imageData = new ImageData(array, width, height)
+  outputImage.width = width
+  outputImage.height = height
+  ctx = outputImage.getContext('2d')
+  ctx.putImageData(imageData, 0, 0)
+}
+
+const onProcessImage = (module, processImage) => () => {
   console.log('[onProcessImage]')
   const { data, width, height } = getImageData()
-  processImage(data, width, height)
+  const dataOutAddr = processImage(data, width, height)
+  const dataOutSize = width * height * 4
+  const dataOut = module.HEAPU8.slice(dataOutAddr, dataOutAddr + dataOutSize)
+  drawOutputImage(dataOut, width, height)
+  module._free(dataOutAddr)
 }
 
 const wrapProcessImage = module => {
+  console.log('[wrapProcessImage]')
   const ident = 'processImage'
-  const returnType = null
+  const returnType = 'number'
   const argTypes = ['array', 'number', 'number']
   const processImage = module.cwrap(ident, returnType, argTypes)
   return processImage
@@ -35,5 +52,5 @@ const wrapProcessImage = module => {
 const init = module => {
   console.log('[init]')
   const processImage = wrapProcessImage(module)
-  processImageBtn.addEventListener('click', onProcessImage(processImage))
+  processImageBtn.addEventListener('click', onProcessImage(module, processImage))
 }
